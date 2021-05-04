2d4a2c1118100d425a06c4b1dd1c60bf906a6a0e
Oscar mejia <oe6@misena.edu.co>
2021-05-04 12:34:24 -0500

    Initial commit

---
+++ src/app/modules/admin/docs/changelog/changelog.ts
@@ -0,0 +1,98 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector       : 'changelog',
+    templateUrl    : './changelog.html',
+    styles         : [''],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChangelogComponent
+{
+    changelog: any[] = [
+
+        // v12.2.0
+        {
+            version    : 'v12.2.0',
+            releaseDate: 'May 01, 2021',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        '(apps/chat) New and improvement version of Chat app',
+                        '(fuse/fullscreen) Added fullscreen toggle component'
+                    ]
+                },
+                {
+                    type: 'Changed',
+                    list: [
+                        '(dependencies) Updated Angular, Angular Material and various other packages',
+                        '(apps/academy) Better error handling on courses that are not exist',
+                        '(apps/academy) Added missing trackBy functions to ngFor loops',
+                        '(apps/mailbox) Removed unused methods',
+                        '(pages/pricing) Improved the spacing of the CTA section on all pricing pages'
+                    ]
+                }
+            ]
+        },
+        // v12.1.0
+        {
+            version    : 'v12.1.0',
+            releaseDate: 'April 26, 2021',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        '(apps/academy) New and improvement version of Academy app',
+                        '(icons) Material Solid icons'
+                    ]
+                },
+                {
+                    type: 'Changed',
+                    list: [
+                        '(dependencies) Updated Angular, Angular Material and various other packages',
+                        '(icons) Updated Heroicons',
+                        '(icons) Updated Material Icons',
+                        '(apps/file-manager) Better grid for File Manager app',
+                        '(layouts/classy) Removed footer for better \'classy\' look'
+                    ]
+                },
+                {
+                    type: 'Fixed',
+                    list: [
+                        '(apps/contacts) Clicking on the checkbox on Tag select panel acts weird'
+                    ]
+                }
+            ]
+        },
+        // v12.0.0
+        {
+            version    : 'v12.0.0',
+            releaseDate: 'April 16, 2021',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        'This is the new major version of the Fuse and it\'s completely different from previous versions with no upgrade path',
+                        'This version requires a clean installation'
+                    ]
+                },
+                {
+                    type: 'Changed',
+                    list: [
+                        'Improved the look and feel',
+                        'Re-wrote the entire template from scratch using Tailwind',
+                        'Removed 99% of the SCSS styles in favor of Tailwind',
+                        'Integrated Angular Material theming with Tailwind'
+                    ]
+                }
+            ]
+        }
+    ];
+
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
